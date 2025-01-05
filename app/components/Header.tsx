f6dd51159e0713cd17fec5f234aabedfebb2033b
blueZircon123 <146864939+ndim99@users.noreply.github.com>
2025-01-05 02:05:31 +0100

    feat: update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -32,24 +32,16 @@ export default function Header() {
         </div>
 
         {/* Mobile Hamburger Icon */}
-        <div className="sm:hidden pt-[7px]">
-          <button className="text-white" onClick={() => setMenuOpen(!menuOpen)}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="w-5 h-5 sm:hidden flex cursor-pointer"
+          fill="#fff"
+        >
+          <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+        </svg>
       </nav>
 
       {/* Mobile Menu */}

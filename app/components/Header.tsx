0ead4715529a5f05d74247a9c6efa861017354aa
blueZircon123 <146864939+ndim99@users.noreply.github.com>
2025-01-10 00:42:17 +0100

    feat: update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -33,7 +33,18 @@ export default function Header() {
       } flex items-center`}
     >
       <nav className="container mx-auto px-6 2xl:py-4 lg:py-3 py-2.5 flex justify-between items-center">
-        <Link href="#hero" className="lg:text-2xl text-xl font-bold text-white">
+        <Link
+          href="#hero"
+          className="lg:text-2xl text-xl font-bold text-white flex items-center gap-2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className="lg:w-6 lg:h-6 w-5 h-5"
+            fill="#fff"
+          >
+            <path d="M96 128a128 128 0 1 0 256 0A128 128 0 1 0 96 128zm94.5 200.2l18.6 31L175.8 483.1l-36-146.9c-2-8.1-9.8-13.4-17.9-11.3C51.9 342.4 0 405.8 0 481.3c0 17 13.8 30.7 30.7 30.7l131.7 0c0 0 0 0 .1 0l5.5 0 112 0 5.5 0c0 0 0 0 .1 0l131.7 0c17 0 30.7-13.8 30.7-30.7c0-75.5-51.9-138.9-121.9-156.4c-8.1-2-15.9 3.3-17.9 11.3l-36 146.9L238.9 359.2l18.6-31c6.4-10.7-1.3-24.2-13.7-24.2L224 304l-19.7 0c-12.4 0-20.1 13.6-13.7 24.2z" />
+          </svg>
           My Portfolio
         </Link>
 
